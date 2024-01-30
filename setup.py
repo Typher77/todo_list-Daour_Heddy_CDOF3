a58fdc2f8fdb66dec5e2887c4bf3598ffdf40713
heddy <120495158+Didy-12@users.noreply.github.com>
2024-01-30 14:48:47 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+  name='{todo-list}',
+  version='1.0',
+  author='{Daour_Heddy}',
+  license='MIT',
+  long_description=open('README.md').read(),
+)
